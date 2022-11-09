280f30dd7093663e33159ee81b3b335d62bf8c0b
MichaelVT <mvontroyer@unibz.it>
2022-11-09 13:10:19 +0100

    Implemented RMIServer

--- code/src/main/java/TCPServer/RMIServer.java
+++ code/src/main/java/TCPServer/RMIServer.java
@@ -1,5 +1,19 @@
 package TCPServer;
 
-public class RMIServer {
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
+public class RMIServer {
+    public static void main(String[] args) throws RemoteException {
+        try {
+            LocateRegistry.createRegistry(7896);
+            Chat chat = new Chat();
+            Naming.rebind("rmi://localhost/127.0.0.1/chat", chat);
+            System.out.println("RMI Server is ready");
+        } catch(Exception e){
+            System.out.println("RMI Server failed: " + e);
+            e.printStackTrace();
+        }
+    }
 }
