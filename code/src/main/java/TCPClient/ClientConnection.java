d845d5425c75cb8bb187e5eabc67607d6cf43205
Francesco <francesco.mazzini432@gmail.com>
2022-11-09 11:06:33 +0100

    Added getMessage from client side
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- code/src/main/java/TCPClient/ClientConnection.java
+++ code/src/main/java/TCPClient/ClientConnection.java
@@ -2,14 +2,22 @@ package TCPClient;
 import TCPServer.ChatInterface;
 import java.io.IOException;
 import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ClientConnection {
 
     ChatInterface a;
+    List<String> chatMessages;
+
+    int indexChat;
 
     public ClientConnection(String clientName, String ipServer, int portServer) throws Exception {
         a = (ChatInterface) Naming.lookup("rmi://localhost/ABC");
         a.joinChat(ipServer, portServer, clientName);
+        chatMessages = new ArrayList<>();
+        indexChat = 0;
     }
 
     public void sendMessage (String message) {
@@ -21,14 +29,17 @@ public class ClientConnection {
     }
 
     public String getMessage () {
-        /*
-        String message = "";
+
         try {
-            message = in.readUTF();
-        } catch (IOException e) {
-            System.out.println("IO: "+e.getMessage());
-        } */
-        return null;
+            chatMessages = a.getChat(indexChat);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        String message = String.join("\n", chatMessages);
+        indexChat = chatMessages.size();
+
+        return message;
     }
 
     /**
